f30ef310d21cc3071b076ef6445f1018d6ce7f91
mugilan1505 <mugilanmugilan1505@gmail.com>
2025-07-15 10:31:37 +0530

    Day 9 task

--- springboot-react-ems/src/components/Login.jsx
+++ springboot-react-ems/src/components/Login.jsx
@@ -1,49 +1,39 @@
 import { useState } from "react";
 import axios from "axios";
-
 const Login = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  async function handleLogin(event){
-    event.preventDefault();
-    try{
-        const token = await axios.post("http://localhost:3001/api/auth/login",{userName,password})
-        console.log(token);
-        alert("Login Successful")
-    } catch (e){
-        console.log("Login Error", e);
-        alert("Invalid Cred")
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    async function handleLogin(event) {
+        event.preventDefault();
+        try{
+            const request = await axios.post("http://localhost:3004/api/auth/login", {
+                userName,
+                password    
+            })
+            const token = request.data;
+            console.log(token);
+            localStorage
+            alert("Login Successful");
+        }catch(e){
+            console.log("Login Error", e);
+        }
+        console.log("From Submitted");
     }
-    console.log("Form Submitted");
-  }
-  return (
-    <div>
-      <h2>Login</h2>
-      <div>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="userName">User Name</label>
-          <input
-            id="userName"
-            name="userName"
-            value={userName}
-            type="text"
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <br /> <br />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            value={password}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <br />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div>
+            <h1> Login </h1>
+            <div>
+                <form onSubmit={handleLogin}>
+                    <label htmlFor="userName">User Name</label>
+                    <input  id="userName" name="userName" type="text" onChange={(e) => setUserName(e.target.value)}/>
+                    <br></br>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+                    <br></br>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}; 
 export default Login;
